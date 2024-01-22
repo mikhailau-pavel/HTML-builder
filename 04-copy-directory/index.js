f286bb1bed9ef12d6f7f017bb8343b9cb9641524
github-username <mikhailau.pavel@gmail.com>
2024-01-22 21:26:22 +0300

    fix: add dir removal for copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,12 +5,12 @@ const completePath = path.join(__dirname, 'files')
 const copyDir = async () => {
   try {
     const files = await fs.readdir(completePath, { withFileTypes: true })
+    await fs.rm(path.join(__dirname, 'files-copy'), { force: true, recursive: true })
     await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
-    console.log('Directory created')
 
     files.forEach(async (el) => {
       if (el.isFile()) {
-        await fs.copyFile(`./04-copy-directory/files/${el.name}`, `./04-copy-directory/files-copy/${el.name}`)
+        await fs.copyFile(path.join(completePath, el.name), `${completePath}/../files-copy/${el.name}`)
       }
     })
   }
